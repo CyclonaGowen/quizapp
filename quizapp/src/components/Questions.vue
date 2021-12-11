bd5ffebfe84dc9ba28d77cc40d418ece77ccbf79
CyclonaGowen <cgowen@dialecticapps.com>
2021-12-11 10:52:34 -0600

    rendering Questions and Results

---
+++ quizapp/src/components/Questions.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div class="bar"></div>
+      <div class="status">Dummy Data 1 out of 3 questions answered</div>
+    </div>
+    <div class="single-question">
+      <div class="question">Dummy Data Sample Question 1</div>
+      <div class="answers">
+        <div class="answer">Dummy Data Sample Answer 1</div>
+        <div class="answer">Dummy Data Sample Answer 2</div>
+        <div class="answer">Dummy Data Sample Answer 3</div>
+        <div class="answer">Dummy Data Sample Answer 4</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
